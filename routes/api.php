99fff3ef741c2c5196529d2094ce9b3f01d57810
NicollasFrazao <nicollasfrazao@hotmail.com>
2025-06-08 15:35:55 -0300

    feat: create videos routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('videos')
+    ->controller(\App\Http\Controllers\VideoController::class)
+    ->group(function () {
+        Route::post('/', 'store');
+    });
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('videos.create');
+    //return view('welcome');
 });
+
+Route::prefix('videos')
+    ->controller(\App\Http\Controllers\VideoController::class)
+    ->group(function () {
+        Route::get('/create', 'create')->name('videos.create');
+    });
